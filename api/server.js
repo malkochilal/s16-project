b53e85dbc92cadd29b847e2e63aa617d9b0f0c87
malkochilal <119036086+malkochilal@users.noreply.github.com>
2023-03-14 23:51:04 +0300

    server up and running

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const server = express();
+
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: "not found" })
+})
+
+
+
+
+module.exports = server; 
